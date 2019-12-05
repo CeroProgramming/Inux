459e70b3b6ae3d813add054fc072ef7287578adc
CeroProgramming <programming@quantcero.org>
2019-12-05 13:40:12 +0100

    argument parser and startup script

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
+#!/usr/bin/python3
+
 from evdev import InputDevice, categorize, ecodes, list_devices
 from asyncio import ensure_future, get_event_loop
 from configparser import ConfigParser
 from exec import execute
+from argparse import ArgumentParser
+from os.path import isfile
+
+class NoValidConfig(Exception):
+    pass
+
+parser = ArgumentParser(description='Linux Multiple Keyboard Input Controller')
+parser.add_argument('--config', type=str, help='Pass the config file. Required!')
+
+args = parser.parse_args()
+configfp = vars(args)['config']
+
+if not configfp:
+    raise NoValidConfig('No config passed. Use flag --config <fp>.')
+if not isfile(configfp):
+    raise NoValidConfig('Passed file "%s" not found..' % (configfp,))
 
 
 async def print_events(device):
@@ -11,7 +29,7 @@ async def print_events(device):
 
 
 config = ConfigParser()
-config.read('config/config.ini')
+config.read(configfp)
 
 keyboards = config['DEFAULT']['keyboards'].split(',')
 devices = list()
@@ -19,7 +37,7 @@ devices = list()
 for keyboard in keyboards:
     id, code = execute("xinput --list --id-only '%s'" % (keyboard,))
     if code != 0:
-        print("Keyboard with name '%s' not found. Skipping.." , (keyboard,))
+        print("Keyboard with name '%s' not found. Skipping.." % (keyboard,))
         continue
     execute("xinput float %s" % (id,))
 
